2c5cc144563dd284448f95e416458ea116c4a28b
Roberto Ramirez <robert.624@hotmail.com>
2023-11-16 20:08:33 -0500

    feat; display random quote on load and btn click

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,84 @@
-import { createSignal } from "solid-js";
+import { createSignal, onMount } from "solid-js";
 import { RandomButton, CopyButton } from "./Buttons.jsx";
 import "./App.css";
 
-function App() {
+const baseURL = "https://api.quotable.io";
 
-    const [quote, setQuote] = createSignal("");
+function App() {
+    const [quote, setQuote] = createSignal({});
 
     const handleCopyToClipboard = () => {
-        navigator.clipboard.writeText(quote());
+        navigator.clipboard.writeText(quote().content);
+        alert("Copied to clipboard");
     };
 
-    const handleGetRandomQuote = () => {};
+    const handleGetRandomQuote = () => {
+        // calling the API
+        let url = `${baseURL}/quotes/random`;
 
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => {
+                setQuote(data[0]);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+
+        // reset the scroll position
+        window.scrollTo(0, 0);
+    };
+
+    // Get random quote on load
+    onMount(() => {
+        // calling the API
+        let url = `${baseURL}/quotes/random`;
+
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => {
+                setQuote(data[0]);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    });
 
     return (
         <>
             <main>
                 <div className="main-container">
-                  <div className="wrapper">
-                      <div className="top">
-                          <p className="author">George Bernard Shaw</p>
-                          <div className="categories">
-                              <p>Famous Quotes</p>
-                              <p>Inspirational</p>
-                          </div>
-                      </div>
-                      <p className="quote">
-                          "Learn from yesterday, live for today, hope for
-                          tomorrow."
-                      </p>
-                  </div>
-                  <div className="controls">
-                      <button onClick={handleGetRandomQuote} id="random" className="random">
-                        <RandomButton />
-                      </button>
-                      <button onClick={handleCopyToClipboard} id="copy" className="copy">
-                        <CopyButton />
-                      </button>
-                  </div>
+                    <div className="wrapper">
+                        <div className="top">
+                            <p className="author">{quote().author}</p>
+                            <ul className="categories">
+                                {quote().tags && quote().tags.length > 0 ? (
+                                    quote().tags.map((tag, index) => (
+                                        <li key={index}>{tag}</li>
+                                    ))
+                                ) : (
+                                    <li>No hay tags disponibles</li>
+                                )}
+                            </ul>
+                        </div>
+                        <p className="quote">{quote().content}</p>
+                    </div>
+                    <div className="controls">
+                        <button
+                            onClick={handleGetRandomQuote}
+                            id="random"
+                            className="random"
+                        >
+                            <RandomButton />
+                        </button>
+                        <button
+                            onClick={handleCopyToClipboard}
+                            id="copy"
+                            className="copy"
+                        >
+                            <CopyButton />
+                        </button>
+                    </div>
                 </div>
             </main>
         </>
